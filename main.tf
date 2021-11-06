852c91414fbdabcebb90667e6658a974a79967ba
elianocosta <33909902+elianocosta@users.noreply.github.com>
2021-11-06 20:30:18 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 }
 
 resource "aws_key_pair" "local" {    
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD54ZijkcXcDo+AarBhkJ3IUafyhU+v4htp4WeQPMQy8HfzRoHXBNSqLV039Hf8vlemhNCkrOBHrdFOjjRPG6g5vNpRv5NYtRuY8NYOPSJTh/1lI+FvwlhIlQfzZGl+SA+AK6W5tK/cQR/goq9es9eL604OTyL43wbaMmVx73+PKtH2YMeh9f/Hr0sVJKC6SQpfYW7suv+2oA24tcArMjDlOwB8+gftQ76DrZ99ZAwy/Blc5CbLGvAXI0ZPTVkkR7t/Z6/KR78LAb0dHatzz95MH2omK/TukYk7d+mlzA/FPPEi/jMufgI2QOPoIKm0KTvBxi1HANVmijtw7etdqJ0L eliano@astin-1877429"
+  public_key = ""
 }
 resource "aws_security_group" "click-server" {
 
